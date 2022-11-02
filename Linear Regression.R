bc473f6c2c038a9d3b7e69d74dd36c41196259e2
abhishek-minz <115890200+abhishek-minz@users.noreply.github.com>
2022-11-02 01:29:45 +0000

    Update

--- Linear Regression.R
+++ Linear Regression.R
@@ -1,3 +1,4 @@
+#n is the sample size for the regression analysis
 n<-50
 a<-20
 b<-5
@@ -10,13 +11,19 @@ meany <- a+b*meanx
 sdy <- abs(b)*meanx
 ylim <- c(meany-abs(b)*sdy, meany+abs(b)*sdy)
 
+#Error is necessary in the regression equation
+
 Error <- function(k)
-{ rnorm(n, mean=0, sd=k*abs(b)) }
+    {
+    rnorm(n, mean=0, sd=k*abs(b))
+    }
 
 corr.title <- function(x, y)
-{ paste('Correlation r=', round(cor(x,y),3), 
+{
+    paste('Correlation r=', round(cor(x,y),3), 
         '  r²=', round(cor(x,y)^2,3), sep='') 
 }
+#Here r is equal to 0.98
 
 set.seed(2)
 x <- rnorm(n, mean=meanx, sd=sdx)
@@ -24,3 +31,7 @@ y <- a+b*x+Error(1)
 m <- lm(y~x)
 plot(y~x, main=corr.title(x, y))
 abline(m, col='red')
+
+summary(m)
+#par(mfrow=c(2,2))
+#plot(lm(y~x))
